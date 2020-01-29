0173760bfc22dbe0cfbef2c8ff8e80f427deea87
ezedapena <59580992+ezedapena@users.noreply.github.com>
2020-01-29 17:45:55 -0300

    checkbox working

--- Modulo 2/Task 2/js/main.js
+++ Modulo 2/Task 2/js/main.js
@@ -3,17 +3,29 @@ const members = data.results[0].members
 const tbody = document.querySelector("tbody")
 
 
+function functionFilter(){
+    tbody.innerHTML = ""
+    let check = document.getElementsByClassName("congress");
+    for(let i = 0 ; i < check.length ; i++){
+        if ( check[i].checked ){
+            members.filter(e => e.party == check[i].value).forEach(member =>{
 
-members.forEach(member =>{
-    let row = tbody.insertRow(-1);
-    let fullname = member.first_name + " " + ( member.middle_name ||  "" ) + " " + member.last_name
-    fullname = (member.url != "" ? `<a href="`+ member.url + `">` + fullname + `</a>` : fullname )
-     
-    row.innerHTML = `
-    <td> ${fullname}</td>
-    <td>${member.party} </td>
-    <td> ${member.state}</td>
-    <td>${member.seniority} </td>
-    <td> ${member.votes_with_party_pct}\%\</td>
-    `
-})
+                let row = tbody.insertRow(-1);
+                let fullname = member.first_name + " " + ( member.middle_name ||  "" ) + " " + member.last_name
+                fullname = (member.url != "" ? `<a href="`+ member.url + `">` + fullname + `</a>` : fullname )
+                 
+                row.innerHTML = `
+                <td> ${fullname}</td>
+                <td>${member.party} </td>
+                <td> ${member.state}</td>
+                <td>${member.seniority} </td>
+                <td> ${member.votes_with_party_pct}\%\</td>
+                `
+            })
+        }
+    }
+}
+document.getElementById("rep").addEventListener("click",functionFilter)
+document.getElementById("dem").addEventListener("click",functionFilter)
+document.getElementById("ind").addEventListener("click",functionFilter)
+functionFilter()
